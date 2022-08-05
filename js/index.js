9a38a51a6b1bc6192ff10eb66d19e517977b955f
yenwenkuo <108907747+kuouxui@users.noreply.github.com>
2022-08-04 23:13:02 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 console.log("your index.js file is loaded correctly!");
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $(".projectImg").hover()
-    //when the mouse hover
-    function(){
-        $(this).animate({
-            marginTop: "-=1%",
-        },200);
-    },
+//     $(".projectImg").hover()
+//     //when the mouse hover
+//     function(){
+//         $(this).animate({
+//             marginTop: "-=1%",
+//         },200);
+//     },
 
-    //triger when mouse leave
-    function(){
-        $(this).animate({
-            marginTop: "0%",
-        },200);
-    } 
-});
+//     //triger when mouse leave
+//     function(){
+//         $(this).animate({
+//             marginTop: "0%",
+//         },200);
+//     } 
+// });
