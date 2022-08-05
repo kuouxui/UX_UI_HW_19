cd1aacd724b237fe65a08af26bc1374fad4c2759
yenwenkuo <108907747+kuouxui@users.noreply.github.com>
2022-08-04 23:07:52 -0400

    Crunch Time!!

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 console.log("your index.js file is loaded correctly!");
 
-$( ".card" ).hover(//triger when mouse hover
-    function() {
-      $( this ).animate({
-        marginTop: "-=1%",
-      }, 200);
-      
-      //triger when mouse leav
-      function(){
+$(document).ready(function(){
+
+    $(".projectImg").hover()
+    //when the mouse hover
+    function(){
         $(this).animate({
-            marginTop: "0%"
-        }, 200)
-      }
-    }
-  );
+            marginTop: "-=1%",
+        },200);
+    },
+
+    //triger when mouse leave
+    function(){
+        $(this).animate({
+            marginTop: "0%",
+        },200);
+    } 
+});
